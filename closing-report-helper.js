df6f50f39c4e6c572ada1e1a7ea7acc285f0c223
Arthur Taft <137522644+arthur-taft@users.noreply.github.com>
2024-04-30 14:31:36 -0600

    Table Colors, Code Cleanup, Version Bump
    The data entry table now has colors! Cleaned up some code to make it more readable. Bumped the version since I think that we've made enough changes to constitute that, now and in the past.

--- closing-report-helper.js
+++ closing-report-helper.js
@@ -1,11 +1,41 @@
 /* =============================
  * Rides Closing Report Helper
  * - by Jordan Myers
- * - version 1.0.0 (Nov 1 2023)
+ * -             &
+ * - by Arthur Taft
+ * - version 1.3.0 (Apr 30 2024)
  * ============================= */
 
 // Create a globally-scoped object to store timeouts for each fieldset
-const timeoutStore = {};
+const timeoutStore = {};;
+
+// Set the button colors for each area globally
+const areaColor = {
+  1: '#bbf7d0',
+  2: '#fbcfe8',
+  3: '#fef08a',
+  4: '#fda4af',
+  5: '#fed7aa',
+  6: '#facc15',
+  7: '#bfdbfe',
+  8: '#ddd6fe',
+  9: '#51e077',
+  10: '#f55b9b',
+};
+
+// Set button colors using RGBA values for table headers
+const rgbaAreaColor = {
+  1: 'rgba(187, 247, 208, 0.5)',
+  2: 'rgba(251, 207, 232, 0.5)',
+  3: 'rgba(254, 240, 138, 0.5)',
+  4: 'rgba(253, 164, 175, 0.5)',
+  5: 'rgba(254, 215, 170, 0.5)',
+  6: 'rgba(250, 204, 21, 0.5)',
+  7: 'rgba(191, 219, 254, 0.5)',
+  8: 'rgba(221, 214, 254, 0.5)',
+  9: 'rgba(81, 224, 119, 0.5)',
+  10: 'rgba(245, 91, 155, 0.5)',
+};
 
 // Handle swapping beginning/ending counts when clicking an eligible field
 function clickToSwap(rideId) {
@@ -148,6 +178,44 @@ function injectDifferenceFields() {
   console.log('Difference Field Injected');
 }
 
+function setAreaColors() {
+  // Set the current area color to a more readable variable
+  let currentAreaColor = areaColor[area.value];
+
+  // Use the rgba area colors for the light background
+  let currentAreaColorLight = rgbaAreaColor[area.value];
+  console.log(currentAreaColor);
+  
+  // Get all the headers in the data entry table
+  const darkElement = document.querySelectorAll('#mytable th').forEach((header) => {
+    // Modify only headers with no class name
+    if (header.className === '') {
+      // Set the background color of the header to the current area color
+      header.style.setProperty('--custom-color', currentAreaColor); 
+    }
+  });
+
+  // Get all the table data elements in the table
+  const lightElement = document.querySelectorAll('#mytable td').forEach((data) => {
+    // Modify only data elements with the 'alt' class name
+    if (data.className === 'alt') {
+      // Set the background color of the data element to the light version of the current area color
+      data.style.setProperty('--custom-color-light', currentAreaColorLight);
+    }
+  });
+}
+
+// Use an observer to make sure the colors don't load before the other page information
+// IF YOU GET RID OF THIS THE TABLE COLORS WILL NOT LOAD
+function runAreaColors() {
+  // Get the entry form
+  const dataForm = document.querySelector('#dataForm');
+  // Create an observer for the form
+  const formObserver = new MutationObserver(setAreaColors);
+  // Observe the form for changes to the child nodes (i.e., when the area has been changed)
+  formObserver.observe(dataForm, { childList: true });
+}
+
 // To be called on DOM load
 function runDifferenceObserver() {
   // Get the entry form
@@ -162,24 +230,11 @@ function runDifferenceObserver() {
 document.body.onload = () => {
   // Run the difference injector/observer
   runDifferenceObserver();
+  runAreaColors();
 
   // Some style changes to the area selector and difference fields
   const originalAreaSelector = document.querySelector('select#area');
   const originalAreaOptions = document.querySelectorAll('select#area option');
-
-  // Set the button colors for each area
-  const areaColor = {
-    1: '#bbf7d0',
-    2: '#fbcfe8',
-    3: '#fef08a',
-    4: '#fda4af',
-    5: '#fed7aa',
-    6: '#facc15',
-    7: '#bfdbfe',
-    8: '#ddd6fe',
-    9: '#51e077',
-    10: '#f55b9b',
-  };
 
   // Create a wrapper div for the button group and insert it
   const buttonsDiv = Object.assign(document.createElement('div'), {
@@ -204,7 +259,7 @@ document.body.onload = () => {
       style: `background-color: ${areaColor[area.value]};`,
     });
     areaButton.setAttribute('class', area.selected ? 'active' : ''); // Make the default selection active on load
-
+    
     // Add a click listener to spoof the original selector
     // (utilizing the built-in onchange function)
     areaButton.addEventListener('click', () => {
@@ -222,6 +277,20 @@ document.body.onload = () => {
     buttonsDiv.insertAdjacentElement('beforeend', areaButton);
   });
 
+  window.CSS.registerProperty({
+    name: "--custom-color",
+    syntax: "<color>",
+    inherits: false,
+    initialValue: 'rgba(0, 0, 0, 0.2)',
+  });
+
+  window.CSS.registerProperty({
+    name: "--custom-color-light",
+    syntax: "<color>",
+    inherits: false,
+    initialValue: 'rgba(0, 0, 0, 0.05',
+  });
+  
   // Inject an inline stylesheet
   const selectorStyle = Object.assign(document.createElement('style'), {
     innerHTML: `
@@ -275,6 +344,7 @@ document.body.onload = () => {
       }
     `,
   });
+
   // Hijack the present data form and get rid of the horrible formatting by injecting an inline stylesheet
   const dataTableStyle = Object.assign(document.createElement('style'), {
     innerHTML: `
@@ -283,70 +353,63 @@ document.body.onload = () => {
       }
 
       #mytable th {
+        color: rgb(0, 0, 0);
+        background: none;
+        background-color: var(--custom-color);
+        border-collapse: collapse;
+        border-bottom: 1px solid rgba(0, 0, 0, 0.3);
+        border-right: 1px solid rgba(0, 0, 0, 0.3);
+        border-top: 1px solid rgba(0, 0, 0, 0.3);
         font-size: 14px;
         font-weight: bold;
-        border-right: 1px solid rgba(0, 0, 0, 0.3);
-        border-bottom: 1px solid rgba(0, 0, 0, 0.3);
-        border-top: 1px solid rgba(0, 0, 0, 0.3);
         letter-spacing: 1px;
-        text-transform: none;
-        text-align: center;
         padding: 4px;
-        background: none;
-        background-color: rgba(0, 0, 0, 0.3);
-        border-collapse: collapse;
-        color: rgb(0, 0, 0);
+        text-align: center;
+        text-transform: none;
       }
 
       #mytable th.nobg {
-        border-right: 1px solid rgba(0, 0, 0, 0.3);
         background: none;
         background-color: rgba(0, 0, 0, 0.1);
-        border-top-left-radius: 8px;
         border-collapse: separate;
+        border-right: 1px solid rgba(0, 0, 0, 0.3);
+        border-top-left-radius: 8px;
       }
 
       #mytable th.spec {
-        border-top: 1px solid rgba(0, 0, 0, 0.3);
+        background: none;
         border-left: 1px solid rgba(0, 0, 0, 0.3);
         border-right: 1px solid rgba(0, 0, 0, 0.3);
-        background: none;
+        border-top: 1px solid rgba(0, 0, 0, 0.3);
         border-collapse: collapse;
         border-radius: 0px;
         font-size: 16px;
       }
 
       #mytable th.specalt {
-        border-left: 1px solid rgba(0, 0, 0, 0.3);
-        border-right: 1px solid rgba(0, 0, 0, 0.3);
+        color: rgba(0, 0, 0, 0.7);
         background: none;
         border-collapse: collapse;
-        color: rgba(0, 0, 0, 0.7);
+        border-left: 1px solid rgba(0, 0, 0, 0.3);
+        border-right: 1px solid rgba(0, 0, 0, 0.3);
         font-size: 16px;
       }
 
       #mytable th:last-child {
-        border-top-right-radius: 8px;
         border-collapse: separate;
+        border-top-right-radius: 8px;
       }
 
       #mytable td {
-        border-right: 1px solid rgba(0, 0, 0, 0.3);
-        border-bottom: 1px solid rgba(0, 0, 0, 0.3);
         background: none;
+        border-collapse: collapse;
+        border-bottom: 1px solid rgba(0, 0, 0, 0.3);
+        border-right: 1px solid rgba(0, 0, 0, 0.3);
         padding: 6px 6px 6px 6px;
       }
 
       #mytable td.alt {
-        background-color: rgba(0, 0, 0, 0.05);
-      }
-
-      #mytable td:last-child th:first-child {
-        border-bottom-left-radius: 8px;
-      }
-
-      #mytable td:last-child th:last-child {
-        border-bottom-right-radius: 8px;
+        background-color: var(--custom-color-light);
       }
 
       #mytable:after {
@@ -361,16 +424,16 @@ document.body.onload = () => {
   const submitButtonStyle = Object.assign(document.createElement('style'), {
     innerHTML: `
       input[type=submit] {
-        float: center; 
-        margin-top: 10px;
-        margin-left: 20vw;
-        min-height: 7vh;
-        min-width: 25vw;
-        border-radius: 8px;
-        font-weight: bold;
+        color: rgba(0, 0, 0, 0.6);
         background-color: rgba(0, 0, 0, 0.1);
         border: 2px solid rgba(0, 0, 0, 0.3);
-        color: rgba(0, 0, 0, 0.6);
+        border-radius: 8px;
+        float: center; 
+        font-weight: bold;
+        margin-left: 20vw;
+        margin-top: 10px;
+        min-height: 7vh;
+        min-width: 25vw;
       }
     `
   });
